5b5378c486dba138b664eaea71a664d18106bb65
Bhargav <bhargavkadali39@gmail.com>
2021-12-17 20:44:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+// document.querySelector("button").addEventListener("click", function() {
+//     var myValue = document.getElementById('AppID').value;
+//     var myValue1 = document.getElementById('token').value;
+//     var myValue2 = document.getElementById('channel').value;
+//     alert(myValue);
+//     })
+
+
+var myValue = document.getElementById('userVal').value;
+document.getElementById("Name").value = myValue;
+alert(myValue)
